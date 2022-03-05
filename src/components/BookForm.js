06101f98a1d0c1d7edc97f01198551ae09cf8539
Katie Pundt <kzpundt@gmail.com>
2022-03-04 21:44:58 -0800

    assign users to books

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,16 +1,19 @@
 import { useState } from 'react'
+import { useAuthContext } from '../hooks/useAuthContext'
 // firebase imports
 import { db } from '../firebase/config'
 import { collection, addDoc } from 'firebase/firestore'
 
 export default function BookForm() {
   const [newBook, setNewBook] = useState('');
+  const { user } = useAuthContext();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     await addDoc(collection(db, 'books'), {
-      title: newBook
+      title: newBook,
+      uid: user.uid
     });
 
     setNewBook('');
