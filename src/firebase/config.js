394fa1298d33dc8155048cc458a5cfc91c637a16
Katie Pundt <kzpundt@gmail.com>
2022-03-04 19:26:53 -0800

    add firebase and firestore to project

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAX2IkePJacMoXyX2SsNPwRWaUEhAwPzos",
+  authDomain: "readinglistapp-5b2a5.firebaseapp.com",
+  projectId: "readinglistapp-5b2a5",
+  storageBucket: "readinglistapp-5b2a5.appspot.com",
+  messagingSenderId: "554124290373",
+  appId: "1:554124290373:web:cb781e09592cf2d828362c"
+};
+
+// init firebase
+initializeApp(firebaseConfig);
+
+// init firestore
+const db = getFirestore();
+
+export { db }
