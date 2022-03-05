62537c9f614b9e71c2bb3afd4735060df6f9218a
Katie Pundt <kzpundt@gmail.com>
2022-03-04 20:05:55 -0800

    refactor to use custom useCollection hook

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,10 @@
-import { useState, useEffect } from 'react'
-import { db } from '../firebase/config'
-import { collection, getDocs } from 'firebase/firestore'
+import { useCollection } from '../hooks/useCollection'
 // components
 import BookList from '../components/BookList'
 import BookForm from '../components/BookForm'
 
 export default function Home() {
-  const [books, setBooks] = useState(null);
-
-  useEffect(() => {
-    const ref = collection(db, 'books');
-    
-    getDocs(ref)
-      .then((snapshot) => {
-        let results = [];
-        snapshot.docs.forEach(doc => {
-          results.push({ id: doc.id, ...doc.data()});
-        });
-        setBooks(results);
-      });
-  }, []);
-  
+  const { documents: books } = useCollection('books');
 
   return (
     <div className="App">
